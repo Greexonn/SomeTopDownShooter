61b456222612aba25bfbd09fc6ea2714b1bec1fc
Nikita <smirnyh_ns@zrnc.ru>
2019-04-10 17:50:08 +0300

    Cloth and EnemyController

---
+++ Assets/Scripts/Enemies/SimpleEnemyController.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SimpleEnemyController : MonoBehaviour
+{
+    public float attackDistance;
+
+    private NavMeshAgent _agent;
+    private Animator _animator;
+    private HealthController _health;
+    private GameObject _currentEnemy = null;
+
+    private enum EnemyState
+    {
+        Idle,
+        Follow,
+        Attack
+    }
+    private EnemyState _currentState;
+
+    void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+        _animator = GetComponent<Animator>();
+        _health = GetComponent<HealthController>();
+        _currentState = EnemyState.Idle;
+    }
+
+    void Update()
+    {
+        UpdateState();
+    }
+
+    private void UpdateState()
+    {
+        if (_currentEnemy != null)
+        {
+            UpdateDestination();
+            switch (_currentState)
+            {
+                case EnemyState.Idle:
+                {
+                    //on player found
+                    _currentState = EnemyState.Follow;
+                    //behaviour
+                    SetAnimationState();
+                    break;
+                }
+                case EnemyState.Follow:
+                {
+                    if (Vector3.Distance(_currentEnemy.transform.position, transform.position) <= attackDistance)
+                    {
+                        //on player close enough to attack
+                        _currentState = EnemyState.Attack;
+                        //behaviour
+                        SetAnimationState();
+                    }
+                    break;
+                }
+                case EnemyState.Attack:
+                {
+                    if (Vector3.Distance(_currentEnemy.transform.position, transform.position) > attackDistance)
+                    {
+                        //on player got away from attack
+                        _currentState = EnemyState.Follow;
+                        //behaviour
+                        SetAnimationState();
+                    }
+                    break;
+                }
+            }
+        }
+        else
+        {
+            if (_currentState != EnemyState.Idle)
+            {
+                //on player lost
+                _currentState = EnemyState.Idle;
+                //behaviour
+                _agent.SetDestination(transform.position);
+                SetAnimationState();
+            }
+        }
+    }
+
+    private void SetAnimationState()
+    {
+        //clear
+        _animator.SetBool("Idle", false);
+        _animator.SetBool("Follow", false);
+         _animator.SetBool("Attack", false);
+
+        //set
+        switch (_currentState)
+        {
+            case EnemyState.Idle:
+            {
+                _animator.SetBool("Idle", true);
+                break;
+            }
+            case EnemyState.Follow:
+            {
+                _animator.SetBool("Follow", true);
+                break;
+            }
+            case EnemyState.Attack:
+            {
+                _animator.SetBool("Attack", true);
+                break;
+            }
+        }
+    }
+
+    private void UpdateDestination()
+    {
+        _agent.SetDestination(_currentEnemy.transform.position);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            _currentEnemy = other.gameObject;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+            _currentEnemy = null;
+    }
+
+    void OnParticleCollision(GameObject other)
+    {
+        if (other.CompareTag("ParticleGun"))
+        {
+            //behaviour on got hit
+            _health.DecreaseHealth(5.0f);
+        }
+    }
+}
---
+++ Assets/Scripts/HealthController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthController : MonoBehaviour
+{
+    public float currentHealth;
+    public float maxHealth;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void DecreaseHealth(float damage)
+    {
+        currentHealth -= damage;
+
+        CheckHealth();
+    }
+
+    private void CheckHealth()
+    {
+        if (currentHealth <= 0)
+        {
+            //behaviour
+            Destroy(Instantiate(GameObject.CreatePrimitive(PrimitiveType.Cylinder), transform.position, transform.rotation), 3.0f);
+            Destroy(gameObject);
+        }
+    }
+}
